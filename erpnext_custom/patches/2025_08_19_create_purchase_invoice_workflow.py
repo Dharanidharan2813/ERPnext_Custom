ef1a649ebdcc3dee4cd1203ca783f34f0fdd1c91
vel122 <velmurugan8297@gmail.com>
2025-08-19 18:30:54 +0530

    Workflow

---
+++ erpnext_custom/patches/2025_08_19_create_purchase_invoice_workflow.py
@@ -0,0 +1,29 @@
+import frappe
+
+def execute():
+    if frappe.db.exists("Workflow", "purchase invoice"):
+        return
+
+    workflow = frappe.get_doc({
+        "doctype": "Workflow",
+        "workflow_name": "purchase invoice",
+        "document_type": "Purchase Invoice",
+        "is_active": 1,
+        "override_status": 1,
+        "workflow_state_field": "workflow_state",
+        "states": [
+            {"state": "Draft", "doc_status": 0, "allow_edit": "All", "update_value": "Draft"},
+            {"state": "Submit for Account Manager", "doc_status": 0, "allow_edit": "All", "update_value": "Submit for Account Manager"},
+            {"state": "Pending", "doc_status": 0, "allow_edit": "All", "update_value": "Pending"},
+            {"state": "Approved", "doc_status": 1, "allow_edit": "All", "update_value": "Approved"},
+            {"state": "Rejected", "doc_status": 0, "allow_edit": "All", "update_value": "Rejected"},
+        ],
+        "transitions": [
+            {"state": "Draft", "action": "Submit for Account Manager", "next_state": "Pending", "allowed": "Purchase Manager", "allow_self_approval": 1},
+            {"state": "Pending", "action": "Approve", "next_state": "Approved", "allowed": "Accounts Manager", "allow_self_approval": 1},
+            {"state": "Pending", "action": "Reject", "next_state": "Rejected", "allowed": "Accounts Manager", "allow_self_approval": 1},
+        ]
+    })
+
+    workflow.insert(ignore_permissions=True)
+    frappe.db.commit()
